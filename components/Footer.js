e31e7535b4fee858fc6c2c4b447e9aed7f6660b8
megpara <meganparadowski@gmail.com>
2024-03-12 18:37:39 -0700

    Add mailing list signup

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
+import Link from "next/link"
+
 export default function Footer() {
     return (
         <div className="absolute bottom-0 h-[8vh] flex justify-between items-center text-xs lowercase italic text-[#bab5b1] border-t-[1px] border-[#F0EBE5] border-opacity-20 footer">
-            <div>Mailing list</div>
+            <Link href="/mail">Mailing list</Link>
             <div><a href="mailto:megan@emparadance.com">megan@emparadance.com</a></div>
             <div>&copy; 2024 all rights reserved</div>
         </div>
