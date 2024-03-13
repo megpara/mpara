c4d3c345e2aecb3f5cb305091ae784e65df138a6
megpara <meganparadowski@gmail.com>
2024-03-13 14:38:35 -0700

    Add site title and description and remove 'accessible'

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Empara | A Movement Company Based in Los Angeles</title>
+        <meta
+          name="description"
+          content="Empara is a movement and choreography company focused on creating nontraditional contemporary dance experiences. Empara celebrates other art forms and encourages all bodies to connect to their innate desire to move."
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
--- pages/ethos.js
+++ pages/ethos.js
@@ -7,7 +7,7 @@ export default function Ethos() {
                 <div className="basis-1/3 text-white flex justify-between pt-[24vh] px-4 md:px-8">
                     <div className="basis-1/2 font-display text-4xl tracking-widest">Ethos</div>
                     <div className="basis-1/2 italic text-[#bab5b1] text-sm">
-                    Empara is a movement and choreography company focused on creating nontraditional and accessible contemporary dance experiences. Empara celebrates other art forms and encourages all bodies to connect to their innate desire to move.
+                    Empara is a movement and choreography company focused on creating nontraditional contemporary dance experiences. Empara celebrates other art forms and encourages all bodies to connect to their innate desire to move.
                     <br />
                     <br />
                     The work brings together audiences that may not otherwise experience contemporary dance by hosting performances in unusual settings. Empara works with visceral, dynamic, and theatrical movement that celebrates the unique styles and qualities of each dancer. The company is driven by collaboration - between the dancers, other mediums, and spaces. 
