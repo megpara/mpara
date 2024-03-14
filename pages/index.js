37ed4f969ab336ffaaa0ca9603b880a12256c419
megpara <meganparadowski@gmail.com>
2024-03-13 18:50:04 -0700

    Edit tagline

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="w-screen h-screen relative">
             <video loop autoPlay muted playsInline className="absolute w-screen h-screen top-0 left-0 object-cover" src="wdtease.mov" />
             <div className="absolute bottom-0 w-full h-[12vh] text-white flex flex-col items-center justify-center">
-                <div className="font-display uppercase text-center text-xs">To take possession of the human form</div>
+                <div className="font-display uppercase text-center text-xs">Take possession of the human form</div>
             </div>
             </div>
             {/* <div className="w-full h-full flex items-center justify-center">
