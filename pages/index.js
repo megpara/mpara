767cdc5bcdf2392bd3143bff7d5bfabc0a82deb4
megpara <meganparadowski@gmail.com>
2024-03-22 13:19:18 -0700

    Thumbnail attempt

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <Layout>
             <div className="w-screen h-screen relative">
-            <video loop autoPlay muted playsInline className="absolute w-screen h-screen top-0 left-0 object-cover" src="wdtease.mov" />
+            <video loop autoPlay muted playsInline className="absolute w-screen h-screen top-0 left-0 object-cover" src="wdtease.mov" poster="/thumbnail.jpg" />
             <div className="absolute bottom-0 w-full h-[12vh] text-white flex flex-col items-center justify-center">
                 <div className="font-display uppercase text-center text-xs">Take possession of the human form</div>
             </div>
