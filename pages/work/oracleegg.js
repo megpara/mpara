6f8dd7a5ceecb5a9b0cc98477bdf5c95f2df74a1
megpara <meganparadowski@Megans-MBP.socal.rr.com>
2024-07-23 12:27:57 -0700

    Add oracle egg work page

---
+++ pages/work/oracleegg.js
@@ -0,0 +1,45 @@
+import Layout from "../../components/Layout";
+import Image from "next/image";
+
+const media = [
+    {
+        location: "/work/oracleegg/biancaSkins.mp4",
+        type: "video"
+    },
+    {
+        location: "/work/oracleegg/cloudRemove.mp4",
+        type: "video"
+    },
+    {
+        location: "/work/oracleegg/davonHead.mp4",
+        type: "video"
+    },
+    {
+        location: "/work/oracleegg/walking.mp4",
+        type: "video"
+    },
+    {
+        location: "/work/oracleegg/Rock.mov",
+        type: "video"
+    },
+    {
+        location: "/work/oracleegg/covering.mp4",
+        type: "video"
+    },
+]
+
+export default function OracleEgg() {
+    return (
+        <Layout>
+            <div className="text-center pt-[24vh] px-4 md:px-8 text-[#bab5b1] text-sm italic">Preliminary exploration with sculptures by Leah Piepgras for "Sun Lover" | Oracle Egg DTLA March 2024<br />Dancers: Davon Rashawn, Daphne Fernberger, Maddie Miller, Kevin Lau, Bianca Chung</div>
+            <div className="text-white w-full columns-1 md:columns-3 gap-2 md:gap-4 p-8 pt-[15vh] relative">
+            {media.map((item) => (
+                <div className="mb-8 md:mb-16">
+                {(item.type) == "image" && <Image src={item.location} alt="Oracle Egg image" width={500} height={300}/>}
+                {(item.type) == "video" && <video loop autoPlay muted playsInline src={item.location}/>}
+                </div>
+            ))}
+            </div>
+        </Layout>
+    )
+}
